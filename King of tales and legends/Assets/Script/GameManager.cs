7c4d14e7a63d56760cb68dbb9a36c40fd6f1e8c1
Alexis Guys <alexis.guys@epitech.eu>
2018-11-19 15:36:12 +0900

    Original push and Game manager

---
+++ King of tales and legends/Assets/Script/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    //Game Manager instantiate
+    public static GameManager instance = null;
+
+    //Settings Variable
+    private int gamemode = -1;
+
+    //Player variables
+    private int player_one = -1;
+    private int player_two = -1;
+    private int background = -1;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+}
---
+++ King of tales and legends/Assets/Script/MenuManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour {
+
+    public void btn_test ()
+    {
+        Debug.Log("Hello");
+    }
+}
